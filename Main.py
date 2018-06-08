2bb561d682910e72c72085b4c719219e54e60636
andrewxu19 <39855208+andrewxu19@users.noreply.github.com>
2018-06-08 15:48:32 +0800

    repaired

--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@
 #-*- coding:utf-8 -*-
 
 from Class_Gear import *
+from Optimism import *
 from math import sin,cos,tan,acos
 
-gp=Gear_Pair()  # initializate the gear pair
+gp=Gear_Pair()  # gear pair initialization
 gp.name=input("please input gear pair number:")
 
 # mn,z,z_mate,alpha_n,ha_n,c_n,beta,b,xn,xn_mate,i="e",hand="LH",a_modified=0
@@ -15,8 +16,8 @@ mn=L[0]
 z1=L[1]
 z2=L[2]
 
-if z2<z1:   # ȷ��z1ΪС���֣�z2Ϊ�����
-    print("Z2 should be greater than Z1")
+if z2<z1:   # to check whether z2 must be greater than z1
+    print("Z2 should be greater than Z1, please change the parameter.")
     exit(None)
 
 alpha_n=L[3]
@@ -30,77 +31,81 @@ gp.i=L[10]        # indication of external / internal gear
 hand1=L[11]
 a_modified=L[12]
 
-# �Ժ���ʹ�õĲ������г�ʼ�� parameter initialization for following usage
-a=0             # ���ľ� cender distance
-i1=""           # С�����Ƿ�Ϊ��ݡ��ڳݣ�"e"Ϊ��ݣ�"i"Ϊ�ڳ�
-i2=""           # ������Ƿ�Ϊ��ݡ��ڳݣ�"e"Ϊ��ݣ�"i"Ϊ�ڳ�
-at_modified=0   # ���Ͻ� engagement angle
+# parameter initialization for following usage
+a=0             # cender distance
+i1=""           # external / internal gear, "e" for external, "i" for internal
+i2=""           # external / internal gear, "e" for external, "i" for internal
+at_modified=0   # engagement angle
 
-if gp.i=="e":   # �ڡ���ݣ���������
-    i1="e"
-    i2="e"
-    if hand1=="LH":
-        hand2="RH"
+if gp.i=="e":   # external engagement
+    i1="e"      # gear1 is an external gear
+    i2="e"      # gear2 is an external gear
+    if hand1=="LH": # helix angle of gear1 is left hand
+        hand2="RH"  # helix angle of gear2 is right hand
     else:
         hand2="LH"
 else:
-    i1="e"
-    i2="i"
+    i1="e"      # gear1 is an external gear
+    i2="i"      # gear2 is an internal gear
     if hand1=="LH":
         hand2="LH"
     else:
         hand2="RH"
 
-g1=Helix_Gear(mn, z1, z2, alpha_n, ha_n, c_n, beta, b, xn1, xn2, i1, hand1)  # С���ֳ�ʼ��
-g2=Helix_Gear(mn, z2, z1, alpha_n, ha_n, c_n, beta, b, xn2, xn1, i2, hand2)  # ����ֳ�ʼ��
+g1=Helix_Gear(mn, z1, z2, alpha_n, ha_n, c_n, beta, b, xn1, xn2, i1, hand1)  # gear1 initialization
+g2=Helix_Gear(mn, z2, z1, alpha_n, ha_n, c_n, beta, b, xn2, xn1, i2, hand2)  # gear2 initialization
 
-if a_modified==0:    # �Ѿ�xn1,xn2�����λ�����ľ� xn1,xn2 already known, to calculate a_modified
-    if gp.i=="e":  # ��Ϊ������ external engagement
+if a_modified==0:    # if xn1,xn2 already known, to calculate a_modified
+    if gp.i=="e":  # if external engagement
         gp.at_modified=ainv(2*(xn2+xn1)*tan(alpha_n)/(z2+z1)+inv(g1.alpha_t))
         gp.yt=(z2+z1)*(cos(g1.alpha_t)/cos(gp.at_modified)-1)/2
         gp.yn=gp.yt/cos(beta)
         gp.a_modified=mn*((z2+z1)/2+gp.yt)/cos(beta)
-        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn2+xn1-gp.yn   # ���ľ�䶯ϵ����������ͳһ
-    else:   # ��Ϊ������ internal engagement
+        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn2+xn1-gp.yn   # center distance modification factor
+    else:   # if internal engagement
         gp.at_modified=ainv(2*(xn2-xn1)*tan(alpha_n)/(z2-z1)+inv(g1.alpha_t))
         gp.yt=(z2-z1)*(cos(g1.alpha_t)/cos(gp.at_modified)-1)/2
         gp.yn=gp.yt/cos(beta)
-        gp.a_modified=mn*((z2-z1)/2+gp.yt)/cos(beta)  # ��λ�����ľ�
-        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn2-xn1-gp.yn   # ���ľ�䶯ϵ����������ͳһ     
-else:    # ��֪��λ�����ľ࣬���λϵ�� a_modified already known, to calculate xn_sigma
-    gp.a_modified=a_modified    # ��λ�����ľ�������
-    if gp.i=="e":   # ��Ϊ������ external engagement
+        gp.a_modified=mn*((z2-z1)/2+gp.yt)/cos(beta)  # center distance modified
+        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn2-xn1-gp.yn   # center distance modification factor     
+else:    # if a_modified (center distance modified) already known, to calculate xn_sigma
+    gp.a_modified=a_modified    # gear pair property assignment
+    if gp.i=="e":   # if external engagement
         gp.a=mn*(z1+z2)/2/cos(beta)
         gp.yt=(a_modified-a)/g1.mt
         gp.yn=(a_modified-a)/mn
-        gp.at_modified=acos(a*cos(g1.alpha_t)/a_modified) # ���Ͻ� engagement angle
-        gp.xn_sigma=(z2+z1)*(inv(gp.at_modified)-inv(g1.alpha_t))/2/tan(alpha_n)   # �ܱ�λϵ��
+        gp.at_modified=acos(a*cos(g1.alpha_t)/a_modified) # engagement angle
+        gp.xn_sigma=(z2+z1)*(inv(gp.at_modified)-inv(g1.alpha_t))/2/tan(alpha_n)   # total modification coefficiency
 
-        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=gp.xn_sigma-gp.yn  # ���ľ�䶯ϵ����������ͳһ
+        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=gp.xn_sigma-gp.yn  # center distance modification factor
+
+        x_distribute(g1,g2,gp)  # xn1, xn2 distribution, the parameter should be a class object
         
-    else:   # ��Ϊ������ internal engagement
+    else:   # if internal engagement
         gp.a=mn*(z2-z1)/2/cos(beta)
         gp.yt=(a_modified-a)/g1.mt
         gp.yn=(a_modified-a)/mn
-        gp.at_modified=acos(a*cos(g1.alpha_t)/a_modified) # ���Ͻ� engagement angle
-        gp.xn_sigma=(z2-z1)*(inv(gp.at_modified)-inv(g1.alpha_t))/2/tan(alpha_n) # �ܱ�λϵ��
+        gp.at_modified=acos(a*cos(g1.alpha_t)/a_modified) # engagement angle
+        gp.xn_sigma=(z2-z1)*(inv(gp.at_modified)-inv(g1.alpha_t))/2/tan(alpha_n) # total modification coefficiency
 
-        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn_sigma-yn  # ���ľ�䶯ϵ����������ͳһ
+        gp.delta_y_n=g1.delta_y_n=g2.delta_y_n=xn_sigma-yn  # center distance modification factor
 
-g1.para_calc()  # ����С���������ľ��λϵ����صĳ��ֲ�������ݶ�Բ���ݸ�Բ�������ľ��λϵ����صĳ��ֲ�������ݶ�Բ���ݸ�Բ�� calculate other parameters related to delta_y_n of gear1
-g2.para_calc()  # �������������ľ��λϵ����صĳ��ֲ�������ݶ�Բ���ݸ�Բ�� calculate other parameters related to delta_y_n of gear2
+        x_distribute(g1,g2,gp)  # xn1, xn2 distribution, the parameter should be a class object
 
-if gp.i=="e":   # ������
-    gp.d1_modified=2*a_modified*z1/(z2+z1) # С���ֽ�Բֱ�� pitch diameter of gear1
-    gp.d2_modified=2*a_modified*z2/(z2+z1) # ����ֽ�Բֱ�� pitch diameter of gear2
+g1.para_calc()  # calculate other parameters related to delta_y_n of gear1 (tip / root diameter)
+g2.para_calc()  # calculate other parameters related to delta_y_n of gear2 (tip / root diameter)
+
+if gp.i=="e":   # if external engagement
+    gp.d1_modified=2*a_modified*z1/(z2+z1) # pitch diameter of gear1
+    gp.d2_modified=2*a_modified*z2/(z2+z1) # pitch diameter of gear2
     gp.Epix_a=(z1*(tan(g1.alpha_at)-tan(gp.at_modified))+z2*(tan(g2.alpha_at)-tan(g2.alpha_t)))/2/pi # �����غ϶� face overlap ratio
-else:   # ������
+else:   # if internal engagement
     gp.d1_modified=2*a_modified*z1/(z2-z1) # pitch diameter of gear1
     gp.d2_modified=2*a_modified*z2/(z2-z1) # pitch diameter of gear2
     gp.Epix_a=(z1*(tan(g1.alpha_at)-tan(gp.at_modified))-z2*(tan(g2.alpha_at)-tan(g2.alpha_t)))/2/pi # �����غ϶� face overlap ratio  
 
-gp.Epix_b=b*sin(beta)/mn/pi    # �����غ϶� axial overlap ratio
-gp.Epix_gama=gp.Epix_a+gp.Epix_b     # ���غ϶� total overlap ratio    
+gp.Epix_b=b*sin(beta)/mn/pi    # axial overlap ratio
+gp.Epix_gama=gp.Epix_a+gp.Epix_b     # total overlap ratio    
 
 # the following will check the engagement quality 
 
--- Optimism.py
+++ Optimism.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
-def x_distribute(xn_sigma):
+def x_distribute(g1,g2,gp):
     # distribution of xn_sigma, and optimization
+    # g1, g2, gp should be class object
     
     xn1=xn_sigma/2
     
     xn2=xn_sigma/2
     
-    return xn1,xn2
+    return 0
 
